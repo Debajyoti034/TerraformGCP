b547caaf7db727627c688af1d37554771858b35c
Debajyoti Sen Gupta <debajyotisengupta11@gmail.com>
2022-08-01 16:40:27 +0000

    commiting main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  project = "stately-century-358107"
+  credentials =file("stately-century-358107-4ce3a89f6390.json")
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
